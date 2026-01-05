95f8912c31ff0c7c74fe4bbe685a549ecdcc24db
Ujwala <ujwalap801@gmail.com>
2026-01-05 06:51:43 +0530

    Added Minimum_Cost_TOclimb

---
+++ Minimum_Cost_Climb.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length;
+    let dp =[0,0,];
+
+    for(let i=2;i <=n; i++)
+    {
+        dp[i] = Math.min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+    }
+
+    return dp[n];
+    
+};
--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 ## [ClimbStairs](https://leetcode.com/problems/climbing-stairs/)
 
 ## [Fibanacci](https://leetcode.com/problems/fibonacci-number/s)
+
+## [Minimum_Cost_TOCLIMB](https://leetcode.com/problems/min-cost-climbing-stairs/)
