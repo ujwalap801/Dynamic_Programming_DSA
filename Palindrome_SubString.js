0e2bef69584dde5bb7161aae91502be3ea308f45
Ujwala <ujwalap801@gmail.com>
2026-01-10 13:16:51 +0530

    Added Palindrome_Substring file

---
+++ Palindrome_SubString.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let n = s.length;
+    let dp = Array.from({length:n}, ()=> Array(n). fill(null))
+let ans =0;
+    for(let i=0;i<n;i++)
+    {
+        dp[i][i] = true;
+        ans++;
+        if(i<n-1 && s[i] === s[i+1])
+        {
+         dp[i][i+1] = true;
+         ans++;
+        }
+    }
+
+
+    console.log(ans)
+    
+    // console.log(dp);
+
+
+
+// length 3...n
+
+for(let len=3; len<=n;len++)
+{
+    for(let i=0;i<=n-len;i++)
+    {
+        let j = i+len-1;
+        if(s[i] === s[j] && dp[i+1][j-1])
+        {
+            dp[i][j] = true;
+            ans++;
+        }
+    }
+}
+
+
+return ans;
+};
